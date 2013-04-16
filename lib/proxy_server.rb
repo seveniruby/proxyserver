b47f33f399624a03e509f7128a78706c663274dd
seveniruby <seveniruby@gmail.com>
2013-04-16 09:14:29 +0800

    rename

--- lib/proxy_server.rb
+++ lib/proxy_server.rb
@@ -1,12 +1,12 @@
 require 'eventmachine'
-require "proxyserver/version"
-require "proxyserver/connection"
-require "proxyserver/backend"
-require "proxyserver/proxy_server"
-require "proxyserver/form_proxy"
-require "proxyserver/http_proxy"
-require "proxyserver/stub"
-require "proxyserver/mysql_stub"
+require "proxy_server/version"
+require "proxy_server/connection"
+require "proxy_server/backend"
+require "proxy_server/proxy_server"
+require "proxy_server/form_proxy"
+require "proxy_server/http_proxy"
+require "proxy_server/stub"
+require "proxy_server/mysql_stub"
 
 module ProxyServer
   # Your code goes here...
--- lib/proxy_server/version.rb
+++ lib/proxy_server/version.rb
@@ -1,3 +1,3 @@
-module Proxyserver
+module ProxyServer
   VERSION = "0.0.4"
 end
